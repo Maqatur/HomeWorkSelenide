290e4e1d10d6c1d8103698d09c9991df8c91a707
Maqatur <maqaturs@gmail.com>
2024-05-02 21:15:47 +0400

    Fixed and try again

--- src/test/java/ru/netology/test/selenide/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/test/selenide/AppCardDeliveryTest.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.Clock;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -26,7 +25,7 @@ class AppCardDeliveryTest {
 
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Самара");
-        String planningDate = generateDate(3, "dd.MM.yyyy");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Макаров Геннадий Петрович");
